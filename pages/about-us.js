1ebf74780f480e7bcc7a827aa96e7575e08cd5ba
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-14 00:23:57 +0600

    about us page/route added

---
+++ pages/about-us.js
@@ -0,0 +1,17 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>About us page</h1>
+    </div>
+  );
+};
+
+export default Page;
+
+// export default function Page() {
+//   return (
+//     <div>
+//       <h1>About us page</h1>
+//     </div>
+//   );
+// }
