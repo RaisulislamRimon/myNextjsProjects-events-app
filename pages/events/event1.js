fe6a3e978450ab774b2534d7c33709903bb1cb7f
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-14 23:48:46 +0600

    added title, navbar, footer in event1 page

--- pages/events/event1.js
+++ pages/events/event1.js
@@ -1,7 +1,19 @@
+import Head from "next/head";
+import Footer from "../footer";
+import Navbar from "../shared/navbar";
+
 const Page = () => {
   return (
     <div>
+      <Head>
+        <title>event1 page</title>
+      </Head>
+
+      <Navbar />
+
       <h1 className="text-3xl font-bold">event1 page</h1>
+
+      <Footer />
     </div>
   );
 };
