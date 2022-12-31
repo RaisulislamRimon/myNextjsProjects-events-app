eab1e38598acd029fe2281553c71e6dddeedb24a
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-31 13:29:51 +0600

    update

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,8 +2,14 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header style={{ padding: "20px" }}>
-      <nav>
+    <header>
+      <nav
+        style={{
+          padding: "20px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
         <img style={{ marginRight: "10px" }} src="" alt="" />
         <Link style={{ marginRight: "10px" }} href="/">
           Home
